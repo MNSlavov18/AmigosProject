c450540aaae161e8f8a2e7d58ac4c8861a026e3b
kamen petrov <mnslavov18@codingburgas.bg>
2021-03-13 18:18:45 +0200

    Add project files.

---
+++ AmigosProject/AmigosProject.cpp
@@ -0,0 +1,11 @@
+// AmigosProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+e
