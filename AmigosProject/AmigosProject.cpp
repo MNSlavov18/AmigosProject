acd338c2ab184dfc0afb294a55cd0f2f6a494485
ViktorVelizarov <58163160+ViktorVelizarov@users.noreply.github.com>
2021-03-13 18:37:11 +0200

    add main file to the project

--- AmigosProject/AmigosProject.cpp
+++ AmigosProject/AmigosProject.cpp
@@ -1,11 +1,28 @@
-// AmigosProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <iomanip> 
+#include "Data.h"
+#include "Presentation.h"
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int* studentCount = new int(0);
+    int* maxStudentID = new int(1);
+    STUDENTS  students[10];
+    int* teacherCount = new int(0);
+    int* maxTeacherID = new int(1);
+    TEACHERS  teachers[10];
+    int* schoolCount = new int(0);
+    int* maxSchoolID = new int(1);
+    SCHOOLS  schools[10];
+    int* TeamCount = new int(0);
+    int* maxTeamID = new int(1);
+    TEAMS teams[10];
+    bool doShowMenu;
+
+    do {
+        doShowMenu = mainMenu(schools, *schoolCount, *maxSchoolID, teachers, *teacherCount, *maxTeacherID, students, *studentCount, *maxStudentID, teams, *TeamCount, *maxTeamID);
+    } while (doShowMenu);
+
 }
-
-
